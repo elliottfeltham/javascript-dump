bda0b0ce39822c04d6e923a640d7b9fdba5d2d2a
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-16 21:40:48 +0000

    Add example of for...of loop

---
+++ for-of-loop.js
@@ -0,0 +1,11 @@
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+const dogs = ["Labradour", "Pitbull", "German Shepard", "Pug", "Doberman", "Jack Russel"];
+
+function printArray(array) {
+    for (const list of array) {
+        console.log(list);
+        }
+}
+
+printArray(cats);
+printArray(dogs);
