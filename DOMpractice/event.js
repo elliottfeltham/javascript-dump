d17522b31c532c4c6f29a1fb9eee1f69ccd7f6c7
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-20 17:48:24 +0000

    Add event listeners

---
+++ DOMpractice/event.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  alert("Hello World");
+});
